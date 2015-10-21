fd7d1b5c838452cd3ab29c2ad81ee7ff7bfb95a9
Ros Haitovich <rhaitovich@salesforce.com>
2015-10-21 19:19:33 +0300

    init

---
+++ DemoUnitTestApp/Rooter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUnitTestApp
+{
+    public class Rooter
+    {
+        public Rooter()
+        {
+        }
+
+        // estimate the square root of a number
+        public double SquareRoot(double x)
+        {
+            double estimate = x;
+            double diff = x;
+            while (diff > estimate / 1000)
+            {
+                double previousEstimate = estimate;
+                estimate = estimate - (estimate * estimate - x) / (2 * estimate);
+                diff = Math.Abs(previousEstimate - estimate);
+            }
+            return estimate;
+        }
+
+    }
+}
---
+++ UnitTestLibrary/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using DemoUnitTestApp;
+
+namespace UnitTestLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        [TestMethod]
+        public void BasicTest()
+        {
+            Rooter rooter = new Rooter();
+            double expected = 0.0;
+            double actual = rooter.SquareRoot(expected * expected);
+            double tolerance = .001;
+            Assert.AreEqual(expected, actual, tolerance);
+        }
+
+    }
+}
